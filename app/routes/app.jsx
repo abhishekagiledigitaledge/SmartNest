068619afe68cb3957a8286ffe7f1c60c5afd5473
abhishekagiledigitaledge <abhishek@agiledigitaledge.com>
2026-01-08 02:18:19 +0530

    added host

--- app/routes/app.jsx
+++ app/routes/app.jsx
@@ -1,4 +1,5 @@
-import { Link, Outlet, useLoaderData, useRouteError } from "@remix-run/react";
+// app/routes/app.jsx
+import { Outlet, useLoaderData, useRouteError } from "@remix-run/react";
 import { boundary } from "@shopify/shopify-app-remix/server";
 import { AppProvider } from "@shopify/shopify-app-remix/react";
 import { NavMenu } from "@shopify/app-bridge-react";
@@ -8,34 +9,38 @@ import { authenticate } from "../shopify.server";
 export const links = () => [{ rel: "stylesheet", href: polarisStyles }];
 
 export const loader = async ({ request }) => {
-  await authenticate.admin(request);
+  const { session } = await authenticate.admin(request);
 
-  return { apiKey: process.env.SHOPIFY_API_KEY || "" };
+  const url = new URL(request.url);
+  const host = url.searchParams.get("host");
+
+  if (!host) throw new Error("Missing host");
+
+  return {
+    apiKey: process.env.SHOPIFY_API_KEY,
+    shop: session.shop,
+    host,
+  };
 };
 
 export default function App() {
-  const { apiKey } = useLoaderData();
+  const { apiKey, host } = useLoaderData();
 
   return (
-    <AppProvider isEmbeddedApp apiKey={apiKey}>
-      <NavMenu>
-        <Link to="/app/admin" rel="home">
-          Home
-        </Link>
-        <Link to="/app/onboarding">
-          Onboarding
-        </Link>
-      </NavMenu>
+    <AppProvider apiKey={apiKey} host={host} isEmbeddedApp>
+      <NavMenu
+        navigationLinks={[
+          { label: "Home", destination: "/app/admin" },
+          { label: "Onboarding", destination: "/app/onboarding" },
+        ]}
+      />
       <Outlet />
     </AppProvider>
   );
 }
 
-// Shopify needs Remix to catch some thrown responses, so that their headers are included in the response.
 export function ErrorBoundary() {
   return boundary.error(useRouteError());
 }
 
-export const headers = (headersArgs) => {
-  return boundary.headers(headersArgs);
-};
+export const headers = (args) => boundary.headers(args);
